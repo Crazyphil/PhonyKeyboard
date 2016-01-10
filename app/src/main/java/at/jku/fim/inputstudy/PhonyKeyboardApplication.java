84c6d4b9c17b45e487e33feb7d44acad08fe7b74
Philipp Kapfer <philipp.kapfer@gmx.at>
2016-01-10 18:07:01 +0100

    Added ACRA for crash reporting

--- app/src/main/java/at/jku/fim/inputstudy/PhonyKeyboardApplication.java
+++ app/src/main/java/at/jku/fim/inputstudy/PhonyKeyboardApplication.java
@@ -2,11 +2,24 @@ package at.jku.fim.inputstudy;
 
 import android.app.Application;
 
-public class PhonyKeyboardApplication extends Application {
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
 
+import at.jku.fim.SensitiveConstants;
+
+@ReportsCrashes(
+        formUri = "https://kapferit.cloudant.com/acra-phonykeyboard/_design/acra-storage/_update/report",
+        formUriBasicAuthLogin = "iseareptineateleathellye", // optional
+        formUriBasicAuthPassword = SensitiveConstants.ACRA_REPORT_PASSWORD, // optional
+        reportType = org.acra.sender.HttpSender.Type.JSON,
+        httpMethod = org.acra.sender.HttpSender.Method.PUT
+)
+public class PhonyKeyboardApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        ACRA.init(this);
+
         BackgroundManager.init(this);
     }
 }
